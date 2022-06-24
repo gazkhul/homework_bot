f246ce5fb8f217a48e59aed657ba862d7ca0e050
Mikhail Kolesov <m.m.kolesov@yandex.ru>
2022-06-24 14:20:37 +0300

    pre alpha v. 0.0001

--- homework.py
+++ homework.py
@@ -1,11 +1,29 @@
-...
+import logging
+import os
+import sys
+import time
+
+import requests
+import telegram
+from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Updater
 
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename='homework_bot.log',
+    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+)
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,14 +38,20 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    for status, message in HOMEWORK_STATUSES.items():
+        if status == 'approved':
+            bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        elif status == 'reviewing':
+            bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        elif status == 'rejected':
+            bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    answer = requests.get(ENDPOINT, headers=HEADERS, params=params).json()
+    return answer
 
 
 def check_response(response):
@@ -36,8 +60,8 @@ def check_response(response):
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    homework_name = get_api_answer['homeworks'][0]['lesson_name']
+    homework_status = get_api_answer['homeworks'][0]['status']
 
     ...
 
