35b16a84991bc3ebca71fedae3e42a2bef41e1da
Mikhail Kolesov <m.m.kolesov@yandex.ru>
2022-06-30 15:03:00 +0300

    fix logger

--- homework.py
+++ homework.py
@@ -12,13 +12,6 @@ import exceptions
 
 load_dotenv()
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -28,7 +21,7 @@ ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
@@ -64,28 +57,30 @@ def check_response(response):
     """Проверяет ответ API на корректность и возвращает список работ."""
     if not isinstance(response, dict):
         raise TypeError('Некорректный тип данных.')
-    try:
-        homeworks = response['homeworks']
-        logger.info('Список работ доступен.')
-    except KeyError as error:
-        logger.error(f'Список работ не доступен: {error}')
+    elif 'homeworks' not in response:
+        raise KeyError('Список работ не доступен')
+
+    homeworks = response['homeworks']
+
     if homeworks is None:
-        logger.error('Данные отсутствуют.')
         raise IndexError('Данные отсутствуют.')
-    elif type(homeworks) is not list:
-        logger.error('Некорректный тип данных.')
+    elif not isinstance(homeworks, list):
         raise TypeError('Некорректный тип данных.')
     return homeworks
 
 
 def parse_status(homework):
     """Проверка статуса последней работы."""
+    if 'homework_name' and 'status' not in homework:
+        raise KeyError('Отсутствую данные последней работы')
+
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    verdict = HOMEWORK_STATUSES[homework_status]
-    if homework_status not in HOMEWORK_STATUSES:
-        logger.debug('Неизвестный статус.')
+
+    if homework_status not in HOMEWORK_VERDICTS:
         raise KeyError(f'Неизвестный статус: {homework_status}')
+
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -96,14 +91,23 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     logger.info('Бот запущен.')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     last_err_msg = ''
 
     if not check_tokens():
-        logger.critical('Бот остановлен.')
-        raise SystemExit('Выход из программы.')
+        logger.critical('Отсутствуют переменные окружения. Бот остановлен.')
+        raise SystemExit(
+            'Выход из программы.'
+        )
     logger.info('Токен найден.')
 
     while check_tokens() is True:
@@ -111,7 +115,10 @@ def main():
             response = get_api_answer(current_timestamp)
             logger.info('Получен ответ от сервера.')
             homework = check_response(response)
+            logger.info('Получен статус последней работы.')
             for status in homework:
+                if status not in HOMEWORK_VERDICTS:
+                    logger.debug(f'{status} - неизвестный статус.')
                 message = parse_status(status)
                 send_message(bot, message)
                 logger.info('Сообщение успешно отправлено.')
